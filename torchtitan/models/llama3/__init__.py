f4577f843f88e1c6f5c6e0a3880786b1f569e77a
ofivite <20295221+ofivite@users.noreply.github.com>
2025-07-22 17:53:41 +0200

    cfgs for lr-bs scan

--- torchtitan/models/llama3/__init__.py
+++ torchtitan/models/llama3/__init__.py
@@ -159,6 +159,20 @@ llama3_configs = {
         depth_init=False,
         norm_eps=1e-30,
     ),
+    "1B-Proxy-nt": TransformerModelArgs(
+        dim=256,
+        n_layers=4,
+        n_heads=4,
+        n_kv_heads=4,
+        ffn_dim_multiplier=1,  # need to check
+        multiple_of=64,
+        qk_norm=True,
+        norm_eps=1e-20,
+        rope_theta=10000,
+        depth_init=False,
+        norm_type="np_rmsnorm",
+        norm_everywhere=True,
+    ),
     "1B-Proxy": TransformerModelArgs(
         dim=256,
         n_layers=24,
