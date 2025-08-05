9d387debfa266359ca03e65b06c2e62a8354cdbb
ofivite <20295221+ofivite@users.noreply.github.com>
2025-08-05 14:43:11 +0200

    x4 wider model cfgs

--- torchtitan/models/llama3/__init__.py
+++ torchtitan/models/llama3/__init__.py
@@ -173,6 +173,20 @@ llama3_configs = {
         norm_type="np_rmsnorm",
         norm_everywhere=True,
     ),
+    "1B-Proxy-nt-x4-width": TransformerModelArgs(
+        dim=1024,
+        n_layers=4,
+        n_heads=16,
+        n_kv_heads=16,
+        ffn_dim_multiplier=1.02325581395,  # to match proxy 2,75 MLP ratio
+        multiple_of=64,
+        qk_norm=True,
+        norm_eps=1e-20,
+        rope_theta=10000,
+        depth_init=False,
+        norm_type="np_rmsnorm",
+        norm_everywhere=True,
+    ),
     "1B-Proxy": TransformerModelArgs(
         dim=256,
         n_layers=24,
