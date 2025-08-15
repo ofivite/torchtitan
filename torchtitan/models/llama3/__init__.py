5dc4e4f9d0ee9fc98206318ec8d4caf4815502f6
ofivite <20295221+ofivite@users.noreply.github.com>
2025-08-15 20:28:18 +0200

    add x8 depth cfg

--- torchtitan/models/llama3/__init__.py
+++ torchtitan/models/llama3/__init__.py
@@ -173,6 +173,20 @@ llama3_configs = {
         norm_type="np_rmsnorm",
         norm_everywhere=True,
     ),
+    "1B-Proxy-nt-x8-depth": TransformerModelArgs(
+        dim=256,
+        n_layers=32,
+        n_heads=4,
+        n_kv_heads=4,
+        ffn_dim_multiplier=1,  # need to check
+        multiple_of=64,
+        qk_norm=True,
+        norm_eps=1e-20,
+        rope_theta=10000,
+        depth_init=False,
+        norm_type="np_rmsnorm",
+        norm_everywhere=True,
+    ),
     "1B-Proxy-nt-x4-width": TransformerModelArgs(
         dim=1024,
         n_layers=4,
