768ff99ccecdeeed3ba5c699666e5f4714022474
ofivite <20295221+ofivite@users.noreply.github.com>
2025-08-21 10:45:31 +0200

    configs for x12 width and thai lang

--- torchtitan/models/llama3/__init__.py
+++ torchtitan/models/llama3/__init__.py
@@ -201,6 +201,20 @@ llama3_configs = {
         norm_type="np_rmsnorm",
         norm_everywhere=True,
     ),
+    "1B-Proxy-nt-x12-width": TransformerModelArgs(
+        dim=3072,
+        n_layers=4,
+        n_heads=48,
+        n_kv_heads=48,
+        ffn_dim_multiplier=1.03106692703,  # to match proxy 2,75 MLP ratio
+        multiple_of=64,
+        qk_norm=True,
+        norm_eps=1e-20,
+        rope_theta=10000,
+        depth_init=False,
+        norm_type="np_rmsnorm",
+        norm_everywhere=True,
+    ),
     "1B-Proxy": TransformerModelArgs(
         dim=256,
         n_layers=24,
